b3690cb2ee13c11675bc53fc9715d064021319eb
Ambareesh "Amby" Balaji <ambareeshbalaji@gmail.com>
2017-06-29 21:51:15 -0400

    Finally fixed it

--- main.js
+++ main.js
@@ -1,38 +1,42 @@
 // ==UserScript==
 // @name    Typeracer Bot
-// @namespace Violentmonkey Scripts
+// @namespace Ambyjkl
 // @description A bot that makes you win every time in TypeRacer
 // @author	Ambyjkl
-// @version  0.5
+// @version  0.51
 // @include http*://play.typeracer.com*
-// @require  https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @grant
 // ==/UserScript==
 
-function main() {
-    var allText = jQuery("div.nonHideableWords").text();
-    var words = allText.split(" ");
-    var i = 0;
-    console.log(words[i]);
-    for(var j = 0; j < words[i].length; j++) {
-        $("input.txtInput").val($("input.txtInput").val() + words[i][j]);
-    }
-    $("input.txtInput").keypress(function() {
-        if (++i < words.length){
-            for(var j = 0; j < words[i].length; j++) {
-                $("input.txtInput").val($("input.txtInput").val() + words[i][j]);
-            }
-        }
-    });
+function main(textBox) {
+  const words = Array.from(document.querySelectorAll('span[unselectable=on]')).map(v => v.innerHTML).join('').split(' ');
+  let i = 0;
 
+  for (let j = 0; j < words[i].length; j++) {
+    textBox.value += words[i][j];
+  }
+  textBox.value += ' ';
+
+  textBox.onkeypress = (e) => {
+    if (e.keyCode === 32) {
+      if (++i < words.length){
+        for (let j = 0; j < words[i].length; j++) {
+          textBox.value += words[i][j];
+        }
+      }
+    }
+  };
 }
 
-$(document).ready(function(){
-    $("input.txtInput").ready(function(){
-        console.log("Input ready");
-        $("input.txtInput").click(function() {
-            console.log("Calling main");
-            main();
-        });
-    });
-});
+setInterval(() => {
+  const textBox = document.querySelector('input.txtInput');
+  if (textBox && !textBox.modded) {
+    textBox.modded = true;
+    textBox.onclick = () => {
+      if (!textBox.clicked) {
+        textBox.clicked = true;
+        main(textBox);
+      }
+    };
+  }
+}, 3000);
